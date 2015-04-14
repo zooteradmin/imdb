e6e634c96dea90dfbd1f037c259aa4d61bc9c9e2
divyanshu <admin@zooternetwork.com>
2015-04-15 01:39:05 +0530

    Add movie working

--- api/serializers.py
+++ api/serializers.py
@@ -6,16 +6,29 @@ class DirectorSerializer(serializers.ModelSerializer):
   class Meta:
     model = Director
     fields = ('id', 'name')
+    read_only_fields = ('id',)
 
 class GenreSerializer(serializers.ModelSerializer):
   class Meta:
     model = Genre
     fields = ('id', 'name')
+    read_only_fields = ('id',)
 
 
 class MovieSerializer(serializers.ModelSerializer):
-  genres = serializers.SlugRelatedField(many=True, read_only=True, slug_field='name')
-  director = serializers.SlugRelatedField(many=True, read_only=True, slug_field='name')
+  director = DirectorSerializer()
+  genres = GenreSerializer(many=True)
   class Meta:
     model = Movie
     fields = ('id', 'name', 'imdb_score', 'popularity', 'director', 'genres')
+    read_only_fields = ('id',)
+  def create(self, validated_data):
+    director_data = validated_data.pop('director')
+    genres_data = validated_data.pop('genres')
+    d = Director.objects.create(name=director_data['name'])
+    validated_data['director_id'] = d.pk
+    movie = Movie.objects.create(**validated_data)
+    for data in genres_data:
+      g = Genre.objects.create(name=data['name'])
+      movie.genres.add(g)
+    return movie
