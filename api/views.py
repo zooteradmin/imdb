7f37e00a9a96416862a3066c0143fd2d2376276e
divyanshu <admin@zooternetwork.com>
2015-04-16 15:44:33 +0530

    added auth

--- api/views.py
+++ api/views.py
@@ -5,7 +5,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
-from api.permissions import IsAdminOrReadOnly
 
 
 class MovieList(APIView):
